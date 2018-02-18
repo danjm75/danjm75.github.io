fd4fce1f648052c0814f7a156f5488cbd25070cd
Daniel Mendoza <danjm75@users.noreply.github.com>
2018-02-18 10:54:09 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function PlaySound(soundobject) {
+     var thissound=document.getElementById(soundobject);
+     thissound.play();
+}
+
+function StopSound(soundobject)) {
+     var thissound=document.getElementById(soundobject);
+     thissound.pause();
+     thissound.currentTime = 0;
+}
+
